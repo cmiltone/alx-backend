05a6f570debfe00799a707c49e4fec04d20c9574
Castro Hamiltone <castromiltone@gmail.com>
2024-01-03 20:28:40 +0300

    fix

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""module creates a class BasicCache that inherits from BaseCaching and is a caching system:
+"""module creates a class BasicCache that inherits from
+BaseCaching and is a caching system:
 
 You must use self.cache_data - dictionary from the parent class BaseCaching
 This caching system doesn't have limit
@@ -10,17 +11,17 @@ def get(self, key):
 Must return the value in self.cache_data linked to key.
 If key is None or if the key doesn't exist in self.cache_data, return None.
 """
-BaseCaching = __import__('0-base_caching').BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
-class BasicCache(BasicCache):
+class BasicCache(BaseCaching):
     """a caching system"""
     def put(self, key, item):
         """sets key in cache"""
         if key is None or item is None:
             return
         else:
-            self.cache_data[key] = item   
+            self.cache_data[key] = item
 
     def get(self, key):
         """gets value of cache with key"""
