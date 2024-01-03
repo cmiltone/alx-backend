3df6999d6cbb8152f71eef751e978a0845ed3a5f
Castro Hamiltone <castromiltone@gmail.com>
2024-01-03 20:51:13 +0300

    2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,42 +1,44 @@
 #!/usr/bin/env python3
-"""module Creates a class FIFOCache that inherits from BaseCaching and
-is a caching system:
+"""module Creates a class LIFOCache that inherits from BaseCaching
+and is a caching system:
 
 You must use self.cache_data - dictionary from the parent class BaseCaching
-You can overload def __init__(self): but don't forget to call the parent
-init: super().__init__()
+You can overload def __init__(self): but don't forget to
+call the parent init: super().__init__()
 def put(self, key, item):
 Must assign to the dictionary self.cache_data the item value for the key key.
 If key or item is None, this method should not do anything.
 If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
-you must discard the first item put in cache (FIFO algorithm)
+you must discard the last item put in cache (LIFO algorithm)
 you must print DISCARD: with the key discarded and following by a new line
 def get(self, key):
 Must return the value in self.cache_data linked to key.
-If key is None or if the key doesn't exist in self.cache_data, return None."""
+If key is None or if the key doesn't exist in self.cache_data, return None.
+"""
+
 from collections import OrderedDict
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    """a caching system - FIFO"""
+class LIFOCache(BaseCaching):
+    """a caching system - LIFO"""
     def __init__(self):
-        """init cache"""
+        """inits cache"""
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """adds item to cache"""
+        """sets item in cache"""
         if key is None or item is None:
             return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD: {}".format(last_key))
         self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(False)
-            print("DISCARD: {}".format(first_key))
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        """gets an item with key"""
-        if key is None or key not in self.cache_data:
-            return None
+        """gets item by key from cache"""
         return self.cache_data.get(key, None)
